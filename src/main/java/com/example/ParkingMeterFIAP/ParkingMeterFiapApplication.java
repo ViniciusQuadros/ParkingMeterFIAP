a6eb0872d8eaad4486922e536abdca0b9e60b308
Vinicius Quadros <voa.quadros@gmail.com>
2024-06-24 20:23:00 -0300

    primeiro commit do projeto ParkingMeterFIAP

---
+++ src/main/java/com/example/ParkingMeterFIAP/ParkingMeterFiapApplication.java
@@ -0,0 +1,13 @@
+package com.example.ParkingMeterFIAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingMeterFiapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingMeterFiapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ParkingMeterFIAP/ParkingMeterFiapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ParkingMeterFIAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingMeterFiapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
