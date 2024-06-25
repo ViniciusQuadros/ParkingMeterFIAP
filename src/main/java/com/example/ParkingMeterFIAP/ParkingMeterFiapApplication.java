099c04983851753248348e13e20d3498c1fa6609
Cleiton Melo Da Silva <cleiton.silva@gmmti.com.br>
2024-06-25 18:27:24 -0300

    Adicionando Swagger Doc

--- src/main/java/com/example/ParkingMeterFIAP/ParkingMeterFiapApplication.java
+++ src/main/java/com/example/ParkingMeterFIAP/ParkingMeterFiapApplication.java
@@ -1,9 +1,18 @@
 package com.example.ParkingMeterFIAP;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "API Parking Meter",
+				description = "Tech Challeng - FIAP 2024 - Grupo 18",
+				version = "1.0.0"
+		)
+)
 public class ParkingMeterFiapApplication {
 
 	public static void main(String[] args) {
