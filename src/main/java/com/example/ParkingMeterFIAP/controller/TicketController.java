283a7eb7bac38c3516d0009409d8b50d1f79088d
vando alves <vandoaderson.s@gmail.com>
2024-07-25 19:48:42 -0300

    Update TicketController.java

--- src/main/java/com/example/ParkingMeterFIAP/controller/TicketController.java
+++ src/main/java/com/example/ParkingMeterFIAP/controller/TicketController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @RestController
 @RequestMapping("/api/v1/ParkingMeter/Ticket")
